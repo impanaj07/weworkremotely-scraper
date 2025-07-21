43d3fb802d462824696ea1eb3d57142695c901cb
impanajgowda14@gmail.com <154488357+impanaj07@users.noreply.github.com>
2025-07-21 23:18:45 +0530

    added scraper.py

---
+++ scraper/scraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+URL="https://weworkremotely.com/categories/remote-programming-jobs"
+conn=sqlite3.connect("jobs.db")
+c=conn.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS jobs(title TEXT,link TEXT)''')
+res=requests.get(URL)
+soup=BeautifulSoup(res.text,'html.parser')
+jobs=soup.select(".jobs article")
+for job in jobs:
+    title=job.select_one('span.title')
+    link="https://weworkremotely.com"+job.find('a')['href']
+    if title:
+        c.execute("INSERT INTO jobs(title,link)VALUES(?,?)", (title.text, link))
+conn.commit()
+conn.close()
